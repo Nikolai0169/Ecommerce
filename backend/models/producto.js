b4548b580b3a5cbe2a0801487683c2c42062eee4
EivorKinKest <120607560+Nikolai0169@users.noreply.github.com>
2026-02-13 14:41:04 -0500

    comentarios

--- backend/models/producto.js
+++ backend/models/producto.js
@@ -1,7 +1,7 @@
 /**
- * Modelo de subcategoría para la base de datos utilizando Sequelize
- * Define la estructura de la tabla "subcategorias" y sus relaciones con otros modelos
- * almacena información sobre las subcategorías de productos en la aplicación
+ * Modelo de producto para la base de datos utilizando Sequelize
+ * Define la estructura de la tabla "productos" y sus relaciones con otros modelos
+ * almacena información sobre los productos en la aplicación
  */
 
 //importar DataTypes de Sequelize para definir los tipos de datos de los campos
@@ -9,20 +9,18 @@ const { DataTypes } = require("sequelize");
 
 //Importar la instancia de Sequelize para definir el modelo
 const sequelize = require("../config/database");
-const Categoria = require("./categoria");
-const Producto = require("./producto");
 
 /**
- * Definir el modelo de subcategoría utilizando sequelize.define()
+ * Definir el modelo de producto utilizando sequelize.define()
  * El primer argumento es el nombre del modelo (en singular), el segundo argumento es un objeto que define los campos y sus tipos de datos, y el tercer argumento es un objeto de opciones para configurar el modelo.
- * En este caso, se define un modelo llamado "Subcategoria" con los campos id (clave primaria, auto-incremental), nombre (cadena de texto, no nulo) y descripcion (cadena de texto).
+ * En este caso, se define un modelo llamado "Producto" con los campos id (clave primaria, auto-incremental), nombre (cadena de texto, no nulo) y descripcion (cadena de texto).
  */
 
-const subcategoria = sequelize.define(
-  "subcategoria",
+const Producto = sequelize.define(
+  "Producto",
   {
-    //campos de la tabla subcategorias
-    //Id Identificador único de la subcategoría, es la clave primaria y se auto-incrementa
+    //campos de la tabla productos
+    //Id Identificador único del producto, es la clave primaria y se auto-incrementa
     id: {
       type: DataTypes.INTEGER, //Tipo de dato entero
       primaryKey: true, //Clave primaria
@@ -32,27 +30,68 @@ const subcategoria = sequelize.define(
 
     //Nombre de la subcategoría, es una cadena de texto y no puede ser nulo
     nombre: {
-      type: DataTypes.STRING(100), //Tipo de dato cadena de texto
+      type: DataTypes.STRING(200), //Tipo de dato cadena de texto
       allowNull: false, //No permite valores nulos
-      unique: {
-        msg: "Ya existe una subcategoría con ese nombre", //Mensaje de error personalizado si se intenta crear una subcategoría con un nombre que ya existe
-      },
       validate: {
         notEmpty: {
-          msg: "El nombre de la subcategoría no puede estar vacío", //Mensaje de error personalizado si se intenta crear una subcategoría con un nombre vacío
+          msg: "El nombre del producto no puede estar vacío", //Mensaje de error personalizado si se intenta crear un producto con un nombre vacío
         },
         len: {
-          args: [2, 100], //El nombre debe tener entre 3 y 100 caracteres
-          msg: "El nombre de la subcategoría debe tener entre 2 y 100 caracteres", //Mensaje de error personalizado si se intenta crear una subcategoría con un nombre que no cumple con la longitud requerida
+          args: [2, 200], //El nombre debe tener entre 2 y 200 caracteres
+          msg: "El nombre del producto debe tener entre 2 y 200 caracteres", //Mensaje de error personalizado si se intenta crear un producto con un nombre que no cumple con la longitud requerida
         },
       },
     },
 
-    //descripción de la subcategoría, es una cadena de texto y puede ser nula
+    //descripción del producto, es una cadena de texto y puede ser nula
     descripcion: {
       type: DataTypes.TEXT, //Tipo de dato cadena de texto
       allowNull: true, //Permite valores nulos
     },
+
+    //Precio del producto, es un número decimal con dos decimales y no puede ser nulo
+    precio: {
+      type: DataTypes.DECIMAL(10, 2), //Tipo de dato decimal con 10 dígitos en total y 2 decimales
+      allowNull: false, //No permite valores nulos
+      validate: {
+        isDecimal: {
+          msg: "El precio del producto debe ser un número decimal válido", //Mensaje de error personalizado si se intenta crear un producto con un precio que no es un número decimal válido
+        },
+        min: {
+          args: [0], //El precio debe ser mayor o igual a 0
+          msg: "El precio del producto no puede ser negativo", //Mensaje de error personalizado si se intenta crear un producto con un precio negativo
+        },
+      },
+    },
+
+    //Stock del producto, es la cantidad de productos disponibles en inventario, en numeros enteros
+    stock: {
+      type: DataTypes.INTEGER, //Tipo de dato entero
+      allowNull: false, //No permite valores nulos
+      defaultValue: 0, //Valor por defecto es 0 (sin stock)
+      validate: {
+        isInt: {
+          msg: "El stock del producto debe ser un número entero válido", //Mensaje de error personalizado si se intenta crear un producto con un stock que no es un número entero válido
+        },
+        min: {
+          args: [0], //El precio debe ser mayor o igual a 0
+          msg: "El stock del producto no puede ser negativo", //Mensaje de error personalizado si se intenta crear un producto con un stock negativo
+        },
+      },
+    },
+
+    /**
+     * imagen: Nombre del archivo de imagen del producto, es una cadena de texto que puede ser nula, se utiliza para almacenar el nombre del archivo de la imagen del producto que se sube a través de la API, esta información es útil para mostrar la imagen del producto en la interfaz de usuario y para gestionar las imágenes en el servidor.
+     * Almacenar solo el nombre del archivo en la base de datos en lugar de la ruta completa o la URL de la imagen tiene varias ventajas, como facilitar la gestión de las imágenes en el servidor, permitir cambios en la estructura de almacenamiento sin afectar a la base de datos, mejorar la seguridad al no exponer rutas completas o URLs, y simplificar las consultas a la base de datos al almacenar solo el nombre del archivo.
+     * ejemplo: producto-12345.jpg
+     * la ruta seria uploads/producto-12345.jpg, pero solo se almacena producto-12345.jpg en la base de datos, esto permite cambiar la estructura de almacenamiento de las imágenes sin afectar a la base de datos, por ejemplo, si se decide almacenar las imágenes en una carpeta diferente o en un servicio de almacenamiento en la nube, no sería necesario actualizar la base de datos para reflejar estos cambios.
+     */
+    imagen: {
+      type: DataTypes.STRING(255), //Tipo de dato cadena de texto con un máximo de 255 caracteres
+      allowNull: true, //Permite valores nulos, ya que no todos los productos pueden tener una imagen asociada
+    },
+    
+
 
     /**
      * CategoriaId es una clave foránea que hace referencia a la categoría a la que pertenece esta subcategoría, es un entero que no puede ser nulo
@@ -85,6 +124,7 @@ const subcategoria = sequelize.define(
       defaultValue: true, //Valor por defecto es true (activa)
     },
   },
+
   {
     //Opciones del modelo
     tableName: "subcategorias", //Nombre de la tabla en la base de datos, se especifica para evitar que Sequelize pluralice el nombre del modelo (por defecto, Sequelize pluraliza los nombres de los modelos para crear las tablas, por ejemplo, el modelo "Subcategoria" se pluralizaría a "Subcategorias")
